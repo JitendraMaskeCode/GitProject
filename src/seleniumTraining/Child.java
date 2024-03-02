08c027eea17ae3bf3492361fdb772a7eace70534
Jitendra Maske <jitendra25255@gmail.com>
2024-03-02 12:45:35 +0530

    y commit

--- src/seleniumTraining/Child.java
+++ src/seleniumTraining/Child.java
@@ -3,7 +3,13 @@ package seleniumTraining;
 public class Child {
 	public static void main(String[] args) {
 		
-		System.out.println("Child updated");
+		System.out.println("Child updated 1");
+		System.out.println("Child updated 2");
+		
+	}
+	
+	void m1() {
+		System.out.println("m1 method");
 	}
 
 }
